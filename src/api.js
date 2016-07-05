c7380a73adb17a4a8da6b70773254cd6cc522485
James Wright <james.wright@net-a-porter.com>
2016-07-05 13:52:31 +0100

    WIP command runner

--- src/api.js
+++ src/api.js
@@ -3,8 +3,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const commandValidator = require('./commandValidator');
-const commandRunner = require('./commandRunner');
+const CommandRunner = require('./CommandRunner');
 const router = express.Router();
+const commandRunner = new CommandRunner();
 
 router.use(bodyParser.json());
 router.use(commandValidator);
@@ -19,7 +20,7 @@ router.post('/command', function runCommand(req, res) {
 	commandRunner.begin(req.body.command);
 
 	commandRunner.on('message', message => res.write(`data: ${message}`));
-	commandRunner.on('end', message => res.end(`data: ${message}`));
+	commandRunner.on('end', exitCode => res.end(`data: ${exitCode}`));
 });
 
 module.exports = router;
--- src/commandRunner.js
+++ src/commandRunner.js
@@ -1,4 +1,20 @@
 'use strict';
 
 const EventEmitter = require('events');
+const spawn = require('child_process').spawn;
 
+function CommandRunner() {
+	this.shell = '/bin/sh';
+}
+
+CommandRunner.prototype = Object.create(EventEmitter.prototype);
+
+CommandRunner.prototype.begin = function begin(command) {
+	const childProcess = spawn(command);
+
+	childProcess.stdout.on('data', data => this.emit('message', data));
+	childProcess.stderr.on('data', data => this.emit('error', data));
+	childProcess.on('close', exitCode => this.emit('end', exitCode));
+};
+
+module.exports = CommandRunner;
