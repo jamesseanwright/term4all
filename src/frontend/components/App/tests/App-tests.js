ace2d77703dbb9408e4d04143d96d370360512cb
James Wright <james@jamesswright.co.uk>
2016-07-09 19:19:28 +0100

    WIP client-side tests

---
+++ src/frontend/components/App/tests/App-tests.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const App = require('../index.jsx');
+
+describe('the App component', function () {
+	it('should render a list of commands and their output', function () {
+		const props = {
+			output: [
+				{
+					id: 1,
+					value: 'ls ~'
+				},
+
+				{
+					id: 2,
+					value: 'Documents'
+				}
+			],
+
+			runCommand() {}
+		};
+		
+		return renderComponent(App, props).then(element => {
+			const listItems = element.querySelectorAll('.terminal__output .output__item');
+
+			console.log(listItems);
+		});
+	});
+});
---
+++ test/renderComponent.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const jsdom = require('jsdom');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+global.renderComponent = function renderComponent(Component, props) {
+	return new Promise((resolve, reject) => {
+		const reactElement = React.createElement(Component, props);
+		const markup = ReactDOMServer.renderToString(reactElement);
+
+		jsdom.env(markup, (err, window) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(window.document.body.firstChild);
+			}
+		})
+	});
+};
